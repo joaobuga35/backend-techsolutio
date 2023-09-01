88d146a5b239cb4508b746e36fec8c4228ad20d1
joaobuga35 <joaobuga35@gmail.com>
2023-09-01 17:05:54 -0300

    feat: create table users

--- src/main/java/tech/solutio/api/model/UserData.java
+++
@@ -1,5 +0,0 @@
-package tech.solutio.api.model;
-
-public record UserData(String name, String email, String password) {
-}
-
---
+++ src/main/java/tech/solutio/api/user/User.java
@@ -0,0 +1,24 @@
+package tech.solutio.api.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Table(name = "users")
+@Entity(name = "user")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private String email;
+    private String password;
+}
