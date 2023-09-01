4342c3ffcf97b0c36f1789822283c557ae905043
joaobuga35 <joaobuga35@gmail.com>
2023-08-31 23:14:01 -0300

    feat: add spring boot project

---
+++ src/test/java/tech/solutio/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.solutio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
