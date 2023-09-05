8a152b42313f0ef34c6029e1d3fddaa7f7745c89
joaobuga35 <joaobuga35@gmail.com>
2023-09-05 03:20:12 -0300

    feat: finish api products

---
+++ src/main/java/tech/solutio/api/infra/security/SecurityFilter.java
@@ -0,0 +1,49 @@
+package tech.solutio.api.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import tech.solutio.api.domain.user.repository.UserRepository;
+import tech.solutio.api.service.TokenService;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UserRepository repository;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = recoverToken(request);
+
+        if (tokenJWT != null) {
+            var subject = tokenService.getSubject((String) tokenJWT);
+            var user = repository.findByEmail(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private Object recoverToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
+        }
+
+        return null;
+    }
+}
--- src/main/java/tech/solutio/api/service/TokenService.java
+++ src/main/java/tech/solutio/api/service/TokenService.java
@@ -3,6 +3,7 @@ package tech.solutio.api.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import tech.solutio.api.domain.user.User;
@@ -28,6 +29,19 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException exception){
             throw new RuntimeException("Error generate token", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return com.auth0.jwt.JWT.require(algorithm)
+                    .withIssuer("API Products")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT invalid or expirate!");
         }
     }
 
