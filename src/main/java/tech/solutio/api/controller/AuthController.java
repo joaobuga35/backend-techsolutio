c9e5fa8a11f0e5477f6fa5a6db9d0fad46012c43
joaobuga35 <joaobuga35@gmail.com>
2023-09-05 01:28:33 -0300

    feat: generate token jwt

--- src/main/java/tech/solutio/api/controller/AuthController.java
+++ src/main/java/tech/solutio/api/controller/AuthController.java
@@ -10,18 +10,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.solutio.api.domain.user.User;
 import tech.solutio.api.domain.user.dto.DataLogin;
+import tech.solutio.api.infra.security.DataTokenJWT;
+import tech.solutio.api.service.TokenService;
 
 @RestController
 @RequestMapping("/login")
 public class AuthController {
     @Autowired
     private AuthenticationManager manager;
+
+    @Autowired
+    private TokenService tokenService;
     @PostMapping
     public ResponseEntity login(@RequestBody @Valid DataLogin dataLogin){
-        var token = new UsernamePasswordAuthenticationToken(dataLogin.email(),dataLogin.password());
-        var authentication = manager.authenticate(token);
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dataLogin.email(),dataLogin.password());
+        var authentication = manager.authenticate(authenticationToken);
 
-        return ResponseEntity.ok().build();
+        var tokenJwt = tokenService.generateToken((User) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new DataTokenJWT(tokenJwt));
     }
 }
---
+++ src/main/java/tech/solutio/api/service/TokenService.java
@@ -0,0 +1,37 @@
+package tech.solutio.api.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import tech.solutio.api.domain.user.User;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import static org.springframework.security.config.Elements.JWT;
+
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
+    public String generateToken(User user){
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return com.auth0.jwt.JWT.create()
+                    .withIssuer("API Products")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(expirationDate())
+                    .withClaim("id",user.getId())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Error generate token", exception);
+        }
+    }
+
+    private Instant expirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
